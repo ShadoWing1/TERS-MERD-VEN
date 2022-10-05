10d9078c0c2e84c65a142883ff6bd08e8cad0a79
ShadoWing1 <oyungemer14@gmail.com>
2022-10-05 22:30:46 +0300

    31sjü ,

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def merdiven(n):
+x = int(input(":")) 
+
+def staricaseONE(n):
     for i in range(1, n + 1):
         print(" "*(n-i) + "#"*i)
 
-
-merdiven(5)
+staricaseONE(x)
+print("-"*x)
+staircase = lambda n: [print(" "*(n-i) + "#"*i) for i in range(1, n + 1)]
+staircase(x)
